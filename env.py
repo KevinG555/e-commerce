9a27652b4c4a39d64aa587452895dc421746e49f
Ubuntu <ubuntu@ip-172-31-45-251.ec2.internal>
2019-09-29 15:15:59 +0000

    requirements.txt file added

--- env.py
+++ env.py
@@ -4,3 +4,7 @@ os.environ.setdefault("STRIPE_PUBLISHABLE", "pk_test_QpWP5DADtNCmhb6zbW35pw9S00c
 
 os.environ.setdefault("STRIPE_SECRET", "sk_test_TRo9DOKnui3pDWT9BN11SIXD00V4twoyoV")
 
+os.environ.setdefault("DATABASE_URL", "postgres://gmmlcguueqwltv:5e77f08867504473b713920728337febfccebd8808008fb89c81a81db6e954e6@ec2-46-137-113-157.eu-west-1.compute.amazonaws.com:5432/daamcl2epv0ipc")
+
+os.environ.setdefault("SECRET_KEY", "a=la4#rmjzb$5t#1k1*q)%89@$2lmdkg+3eb3@awx4r$%o#jn#")
+
