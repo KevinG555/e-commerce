72c534ca81b7bf82ea5a436c941f96a90affe1cc
Ubuntu <ubuntu@ip-172-31-45-251.ec2.internal>
2019-09-29 16:28:39 +0000

    third commit

--- checkout/models.py
+++ checkout/models.py
@@ -25,6 +25,3 @@ class OrderLineItem(models.Model):
     def __str__(self):
         return "{0} {1} @ {2}".format(
             self.quantity, self.product.name, self.product.price)
-from django.db import models
-
-# Create your models here.
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -84,8 +84,8 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-#DATABASES = {
-#   'default': {
+# DATABASES = {
+# 'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
