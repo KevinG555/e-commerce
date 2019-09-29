2cf43ed8cc4eb19a84e7a4d681f1a84db91b633a
Ubuntu <ubuntu@ip-172-31-45-251.ec2.internal>
2019-09-29 13:29:57 +0000

    second commit

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ.setdefault("STRIPE_PUBLISHABLE", "pk_test_QpWP5DADtNCmhb6zbW35pw9S00coKItCqp")
+
+os.environ.setdefault("STRIPE_SECRET", "sk_test_TRo9DOKnui3pDWT9BN11SIXD00V4twoyoV")
+
